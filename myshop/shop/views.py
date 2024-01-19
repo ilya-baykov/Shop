88aa58ac1927c2fad0978abb6149ae9155c1ae1a
ilyabaykov <i-baykov@internet.ru>
2024-01-20 00:37:37 +0300

    Добавил urls

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from models import Product, Category
+from .models import Product, Category
 
 
 # Create your views here.
