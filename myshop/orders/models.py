e03f80ff6f1e0125fed8bbe72bab802fb52b782b
ilyabaykov <i-baykov@internet.ru>
2024-01-29 23:55:51 +0300

    Переименовал некоторые поля в моделях Order. Добавил модели в админ панель

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -6,9 +6,9 @@ from shop.models import Product
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
-    second_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    adrress = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -22,7 +22,7 @@ class Order(models.Model):
         ]
 
     def __str__(self):
-        return f'Order {self.id} {self.first_name} {self.second_name}'
+        return f'Order {self.id} {self.first_name} {self.last_name}'
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
